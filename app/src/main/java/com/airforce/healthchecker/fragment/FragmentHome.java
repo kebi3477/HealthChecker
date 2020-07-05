321ebee82c43b9e75e6458547875622be1a42f2d
Kebi <kebi3477@naver.com>
2020-07-05 16:43:23 +0900

    Change string to object
    array<string> -> array<jsonObject>

--- app/src/main/java/com/airforce/healthchecker/fragment/FragmentHome.java
+++ app/src/main/java/com/airforce/healthchecker/fragment/FragmentHome.java
@@ -12,18 +12,22 @@ import androidx.fragment.app.Fragment;
 
 import com.airforce.healthchecker.MainActivity;
 import com.airforce.healthchecker.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class FragmentHome extends Fragment {
 
-    Button runningButton, pushupButton, situpButton;
+    Button runningButton, pushUpButton, sitUpButton;
+    private BottomNavigationView navigationView;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        navigationView = ((MainActivity)getActivity()).findViewById(R.id.navigationView); //하단 네비게이션 활성화
 
         runningButton = (Button) view.findViewById(R.id.runningButton);
         runningButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                navigationView.getMenu().getItem(2).setChecked(true);
                 ((MainActivity)getActivity()).replaceFragment(new FragmentRunning());
             }
         });
