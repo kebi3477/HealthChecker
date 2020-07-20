5676e5a204a7cc8198f05f9aa690f6bae007e6ca
Kebi <kebi3477@naver.com>
2020-07-21 03:10:21 +0900

    Loading making...

--- app/src/main/java/com/airforce/healthchecker/fragment/FragmentGift.java
+++ app/src/main/java/com/airforce/healthchecker/fragment/FragmentGift.java
@@ -27,6 +27,7 @@ public class FragmentGift extends Fragment {
     private TextView giftDistance;
     private ImageView arrow;
     private ArrayList<JSONObject> recodeList;
+    private ImageView giftBox1, giftBox2, giftBox3, giftBox4, giftBox5;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gift, container, false);
@@ -50,6 +51,24 @@ public class FragmentGift extends Fragment {
             }
             arrow.setPadding(Math.round(arrowMargin), 0, 0, 0);
             giftDistance.setText(totalDistance + "km");
+
+            if(totalDistance >= 10f) {
+                giftBox1 = view.findViewById(R.id.giftBox1);
+                giftBox1.setImageResource(R.mipmap.gift);
+            } else if(totalDistance >= 20f) {
+                giftBox2 = view.findViewById(R.id.giftBox2);
+                giftBox2.setImageResource(R.mipmap.gift);
+            } else if(totalDistance >= 20f) {
+                giftBox3 = view.findViewById(R.id.giftBox3);
+                giftBox3.setImageResource(R.mipmap.gift);
+            } else if(totalDistance >= 20f) {
+                giftBox4 = view.findViewById(R.id.giftBox4);
+                giftBox4.setImageResource(R.mipmap.gift);
+            } else {
+                giftBox5 = view.findViewById(R.id.giftBox5);
+                giftBox5.setImageResource(R.mipmap.gift);
+            }
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -59,4 +78,5 @@ public class FragmentGift extends Fragment {
     float lerp(float a, float b, float f) {
         return a + f * (b - a);
     }
+
 }
